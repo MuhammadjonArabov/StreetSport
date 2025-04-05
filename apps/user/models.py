2c90515ae925bb8dec0df61a5edaf55b72884192
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-04-05 11:29:39 +0500

    user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
+class CustomerUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("Not found phone number!")
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(user=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        return self.create_user(phone_number, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = [
+        ("admin", "Admin"),
+        ("owner", "Owner"),
+        ("manager", "Manager"),
+        ("user", "User"),
+    ]
+
+    phone_number = models.CharField(max_length=15, unique=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    emil = models.EmailField(blank=True, null=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="user")
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomerUserManager()
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone_number
