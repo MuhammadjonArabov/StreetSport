d69b7a623cd78ed42cf7b2e744cc0e45f580bc5a
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-04-05 12:10:51 +0500

    common model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from apps.user.models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class Team(BaseModel):
+    name = models.CharField(max_length=225)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    members = models.ForeignKey(CustomUser, related_name="team_members")
+
+    def __str__(self):
+        return self.name
+
+
+class Stadium(BaseModel):
+    name = models.CharField(max_length=225)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    description = models.TextField(blank=True)
+    price_hour = models.DecimalField(max_digits=10, decimal_places=2)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="stadium_owner")
+    manager = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True,
+                                related_name="stadium_manager")
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Bron(BaseModel):
+    class ProviderType(models.TextChoices):
+        CLICK = 'click', _('Click')
+        PAYME = 'payme', _('Payme')
+        CASH = 'cash', _('Cash')
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="bron_user")
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name="bron_team")
+    stadium = models.ForeignKey(Stadium, on_delete=models.CASCADE, related_name="bron_stadium")
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_paid = models.BooleanField(default=False)
+    order_type = models.CharField(
+        max_length=25,
+        choices=ProviderType.choices, default=ProviderType.CASH
+    )
+
+    class Meta:
+        verbose_name = "Bron"
+        verbose_name_plural = "Bron"
+        constraints = [
+            models.UniqueConstraint(
+                fields=['stadium', 'start_time', 'end_time'],
+                name='unique_bron_per_time'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.stadium.name} | {self.start_time}-{self.end_time}"
+
+
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,5 +1,7 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 
@@ -9,7 +11,7 @@ class CustomerUserManager(BaseUserManager):
             raise ValueError("Not found phone number!")
         user = self.model(phone_number=phone_number, **extra_fields)
         user.set_password(password)
-        user.save(user=self._db)
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, phone_number, password, **extra_fields):
@@ -23,18 +25,22 @@ class CustomerUserManager(BaseUserManager):
             raise ValueError("Superuser must have is_superuser=True")
         return self.create_user(phone_number, password, **extra_fields)
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    ROLE_CHOICES = [
-        ("admin", "Admin"),
-        ("owner", "Owner"),
-        ("manager", "Manager"),
-        ("user", "User"),
-    ]
 
-    phone_number = models.CharField(max_length=15, unique=True)
+phone_validator = RegexValidator(
+    regex=r"^\+998\d{9}$", message=_("Phone number don't match"), code='invalid'
+)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    class RoleType(models.TextChoices):
+        ADMIN = 'admin', _('Admin')
+        OWNER = 'owner', _('Owner')
+        MANAGER = 'manager', _('Manager')
+        USER = 'user', _('User')
+
+    phone_number = models.CharField(max_length=15, unique=True, validators=phone_validator)
     full_name = models.CharField(max_length=255, blank=True)
-    emil = models.EmailField(blank=True, null=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="user")
+    email = models.EmailField(blank=True, null=True)
+    role = models.CharField(max_length=10, choices=RoleType, default=RoleType.USER)
     date_joined = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
