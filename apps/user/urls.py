4e10f4c4ed15397576465f7e179b994dc1fda498
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-04-09 22:43:30 +0500

    user test

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
-    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
-    path("change-password/", ChangePasswordAPIView.as_view(), name="change_password"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token-refresh"),
+    path("change-password/", ChangePasswordAPIView.as_view(), name="change-password"),
 ]
 
--- core/settings/base.py
+++ core/settings/base.py
@@ -222,3 +222,12 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+import sys
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    }
