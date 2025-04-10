1f1e31b7df62db90349f51baee6ef3c9902be535
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-04-10 13:07:22 +0500

    user test full

--- apps/user/tests/test_users.py
+++ apps/user/tests/test_users.py
@@ -2,7 +2,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -35,4 +34,43 @@ class UserTest(APITestCase):
         self.access_token = response.data['tokens']['access']
         self.refresh_tokens = response.data['tokens']['refresh']
 
+    def test_user_logout(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer' + self.access_token)
+        response = self.client.post(self.logout_url, data={'refresh': self.refresh_tokens})
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
+    def test_user_change_password(self):
+        new_password = 'password02'
+        data = {
+            'old_password': 'password04',
+            'new_password': new_password,
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer' + self.access_token)
+        response = self.client.post(self.change_password_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        login_data={
+            'phone_number': '+998932004877',
+            'password': new_password,
+        }
+        login_response = self.client.post(self.login_url, login_data, format='json')
+        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
+
+    def test_user_login_invalid_password(self):
+        data = {
+            'phone_number': '+998932004877',
+            'password': 'password09',
+        }
+        response = self.client.post(self.login_url, data, fromat='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_register_phone_number_exists(self):
+        data = {
+            'phone_number': '+998932004877',
+            'full_name': 'Toms Jared',
+            'password': 'password98',
+        }
+        response = self.client.post(self.register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('message', response.data)
+        self.assertEqual(response.data['message'][0], "This phone number is already registered")
